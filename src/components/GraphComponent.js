deacfdac1853d6e30da07e5b7864d3305bfffb7b
Leonardo Lopes <leonardolopesborges@hotmail.com>
2024-10-10 01:20:32 -0300

    update

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState, useMemo } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { db } from '../data/firebaseConfig';
 import { collection, getDocs } from 'firebase/firestore';
-import '../style/GraphComponent.css'; 
-import '../style/styles.css'; 
+import { Carousel } from 'react-bootstrap'; // Importando o carrossel do react-bootstrap
+import '../style/GraphComponent.css';
+import '../style/styles.css';
 
 import {
   Chart as ChartJS,
@@ -12,43 +13,41 @@ import {
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 } from 'chart.js';
 
 // Registrar os componentes do Chart.js
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const GraphComponent = () => {
   const [chartData, setChartData] = useState(null);
-  const [totalAvaliacoes, setTotalAvaliacoes] = useState(0); // Estado para armazenar o número total de avaliações
+  const [itemData, setItemData] = useState(null);
+  const [totalAvaliacoes, setTotalAvaliacoes] = useState(0);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [index, setIndex] = useState(0); // Estado para controlar o carrossel
 
   useEffect(() => {
     const fetchData = async () => {
       const querySnapshot = await getDocs(collection(db, 'avaliacoes'));
-      let totalAvaliacoesCount = querySnapshot.size; // Conta o número de documentos na coleção
 
       const empresas = {
-        Marechal: { design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
-        Pioneira: { design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
-        Piracicabana: { design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
-        'Pioneira/BRT': { design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
-        'São José/BsBus': { design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
-        Urbi: { design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
+        Marechal: {  totalPontos: 0, totalAvaliacoes: 0, design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
+        Pioneira: { totalPontos: 0, totalAvaliacoes: 0, design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
+        Piracicabana: { totalPontos: 0, totalAvaliacoes: 0, design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
+        'Pioneira/BRT': { totalPontos: 0, totalAvaliacoes: 0, design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
+        'São José/BsBus': { totalPontos: 0, totalAvaliacoes: 0, design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
+        Urbi: { totalPontos: 0, totalAvaliacoes: 0, design: 0, ergonomia: 0, ventilacao: 0, assento: 0, iluminacao: 0, pontualidade: 0, limpeza: 0, conforto: 0, custo: 0, atendimento: 0, count: 0 },
       };
 
       querySnapshot.forEach((doc) => {
-        const data = doc.data().ratings; 
+        const data = doc.data().ratings;
 
         for (const [empresa, avaliacoes] of Object.entries(data)) {
           if (empresas[empresa]) {
+            // Calcula o total de pontos acumulados para cada critério
+            const totalEmpresa = Object.values(avaliacoes).reduce((acc, val) => acc + parseInt(val || 0), 0);
+            empresas[empresa].totalPontos += totalEmpresa;
+            empresas[empresa].totalAvaliacoes++;
             empresas[empresa].design += parseInt(avaliacoes.design || 0);
             empresas[empresa].ergonomia += parseInt(avaliacoes.ergonomia || 0);
             empresas[empresa].ventilacao += parseInt(avaliacoes.ventilacao || 0);
@@ -60,73 +59,62 @@ const GraphComponent = () => {
             empresas[empresa].custo += parseInt(avaliacoes.custo || 0);
             empresas[empresa].atendimento += parseInt(avaliacoes.atendimento || 0);
             empresas[empresa].count++;
+
           }
         }
+
+
       });
 
+      // Calcula a média de pontos por empresa
+      const empresasOrdenadas = Object.keys(empresas)
+        .map((key) => ({
+          empresa: key,
+          media: empresas[key].totalAvaliacoes > 0 ? (empresas[key].totalPontos / empresas[key].totalAvaliacoes) : 0,
+        }))
+        .sort((a, b) => b.media - a.media); // Ordena da maior média para a menor
+
       const chartData = {
-        labels: Object.keys(empresas),
-         datasets: [
+        labels: empresasOrdenadas.map((e) => e.empresa),
+        datasets: [
           {
-            label: 'Média de Design',
-            data: Object.values(empresas).map(e => e.design / (e.count || 1)),
+            label: `Número de Avaliações ` + totalAvaliacoes,
+            data: empresasOrdenadas.map((e) => e.media.toFixed(2)), // Formata para 2 casas decimais
             backgroundColor: 'rgba(75, 192, 192, 0.6)',
-          },
-          {
-            label: 'Média de Ergonomia',
-            data: Object.values(empresas).map(e => e.ergonomia / (e.count || 1)),
-            backgroundColor: 'rgba(255, 99, 132, 0.6)',
-          },
-          {
-            label: 'Média de Ventilação',
-            data: Object.values(empresas).map(e => e.ventilacao / (e.count || 1)),
-            backgroundColor: 'rgba(54, 162, 235, 0.6)',
-          },
-          {
-            label: 'Média de Assento',
-            data: Object.values(empresas).map(e => e.assento / (e.count || 1)),
-            backgroundColor: 'rgba(255, 206, 86, 0.6)',
-          },
-          {
-            label: 'Média de Iluminação',
-            data: Object.values(empresas).map(e => e.iluminacao / (e.count || 1)),
-            backgroundColor: 'rgba(153, 102, 255, 0.6)',
-          },
-          {
-            label: 'Média de Pontualidade',
-            data: Object.values(empresas).map(e => e.pontualidade / (e.count || 1)),
-            backgroundColor: 'rgba(255, 159, 64, 0.6)',
-          },
-          {
-            label: 'Média de Limpeza',
-            data: Object.values(empresas).map(e => e.limpeza / (e.count || 1)),
-            backgroundColor: 'rgba(199, 199, 199, 0.6)',
-          },
-          {
-            label: 'Média de Conforto',
-            data: Object.values(empresas).map(e => e.conforto / (e.count || 1)),
-            backgroundColor: 'rgba(123, 159, 245, 0.6)',
-          },
-          {
-            label: 'Média de Custo',
-            data: Object.values(empresas).map(e => e.custo / (e.count || 1)),
-            backgroundColor: 'rgba(100, 100, 100, 0.6)',
-          },
-          {
-            label: 'Média de Atendimento',
-            data: Object.values(empresas).map(e => e.atendimento / (e.count || 1)),
-            backgroundColor: 'rgba(255, 99, 71, 0.6)',
           },
         ],
       };
 
+
+
+      const itemData = {
+        labels: ['Design', 'Ergonomia', 'Ventilação', 'Assento', 'Iluminação', 'Pontualidade', 'Limpeza', 'Conforto', 'Custo', 'Atendimento'],
+        datasets: Object.keys(empresas).map((key, idx) => ({
+          label: key,
+          data: [
+            empresas[key].design,
+            empresas[key].ergonomia,
+            empresas[key].ventilacao,
+            empresas[key].assento,
+            empresas[key].iluminacao,
+            empresas[key].pontualidade,
+            empresas[key].limpeza,
+            empresas[key].conforto,
+            empresas[key].custo,
+            empresas[key].atendimento,
+            // Aqui você pode ajustar para outras métricas se desejar
+          ],
+          backgroundColor: `rgba(${75 + idx * 30}, ${192 - idx * 10}, ${192 - idx * 20}, 0.6)`,
+        })),
+      };
+
       setChartData(chartData);
-      setTotalAvaliacoes(totalAvaliacoesCount); // Define o total de avaliações
+      setItemData(itemData);
+      setTotalAvaliacoes(querySnapshot.size);
     };
 
     fetchData();
 
-    // Verificar o redimensionamento da janela
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
@@ -138,44 +126,52 @@ const GraphComponent = () => {
     };
   }, []);
 
-  const options = useMemo(() => ({
-    responsive: true,
-    indexAxis: isMobile ? 'y' : 'x', // Troca para vertical em dispositivos móveis
-    plugins: {
-      legend: {
-        position: 'top',
+  const options = useMemo(
+    () => ({
+      responsive: true,
+      indexAxis: isMobile ? 'y' : 'x',
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Ranking das Empresas de Transporte - Brasília/DF',
+        },
       },
-      title: {
-        display: true,
-        text: 'Ranking das Empresas de Transporte - Brasília/DF',
+      scales: {
+        x: {
+          beginAtZero: true,
+        },
+        y: {
+          beginAtZero: true,
+        },
       },
-    },
-    animation: false, 
-    scales: {
-      x: {
-        type: isMobile ? 'linear' : 'category', // Ajusta o tipo de eixo baseado no layout
-        beginAtZero: true,
-      },
-      y: {
-        beginAtZero: true,
-      },
-    },
-  }), [isMobile]);
+    }),
+    [isMobile]
+  );
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
 
   return (
     <section className="page-section portfolio" id="graph">
       <div className="container">
-      <span>Total de Avaliações: {totalAvaliacoes}</span> {/* Mostra o número total de avaliações */}
-        <div className="graph-container">
-        
-          {chartData ? (
-            <Bar data={chartData} options={options} />
-          ) : (
-            <p>Carregando dados...</p>
-          )}
-        </div>
+        <Carousel activeIndex={index} onSelect={handleSelect} controls indicators interval={null} >
+          <Carousel.Item>
+            <div className="graph-container">
+              {chartData ? <Bar data={chartData} options={options} /> : <p>Carregando dados...</p>}
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="graph-container">
+              {itemData ? <Bar data={itemData} options={options} /> : <p>Carregando dados...</p>}
+            </div>
+          </Carousel.Item>
+        </Carousel>
       </div>
-    
     </section>
   );
 };
